6750b5d651de511fcf20962d035770429c45411a
evan <cppmain@gmail.com>
2017-04-09 16:34:30 +0800

    change
    Signed-off-by: evan <cppmain@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,24 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'evan'
-SITENAME = u'hi'
+SITENAME = u'浅析编程技术'
 SITEURL = ''
 
 PATH = 'content'
 
+# 时区
 TIMEZONE = 'Asia/Shanghai'
 
+# 日期格式
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
 DEFAULT_LANG = u'zh'
+
+# THEME = 'pelican-themes/bootstrap2'
+THEME = 'my-themes/bootstrap2'
+
+JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+#JINJA_ENVIRONMENT = ['jinja2.ext.i18n']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,14 +30,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 25
 
